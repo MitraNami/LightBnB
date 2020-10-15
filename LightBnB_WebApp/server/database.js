111b1434c7c1be41f50c7ad0fde3e53384d1a06b
Mitra Nami <a.mxnami@gmail.com>
2020-10-15 23:02:02 +0000

    Add addProperrty query

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -153,9 +153,43 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const {title, 
+  description, 
+  owner_id, 
+  cover_photo_url, 
+  thumbnail_photo_url, 
+  cost_per_night, 
+  parking_spaces, 
+  number_of_bathrooms, 
+  number_of_bedrooms, 
+  province, 
+  city, 
+  country, 
+  street, 
+  post_code} = property;
+
+
+  const values = [
+  title, 
+  description, 
+  owner_id, 
+  cover_photo_url, 
+  thumbnail_photo_url, 
+  cost_per_night, 
+  parking_spaces, 
+  number_of_bathrooms, 
+  number_of_bedrooms, 
+  province, 
+  city, 
+  country, 
+  street, 
+  post_code
+  ];
+
+  return pool.query(`
+    INSERT INTO properties (title, description, owner_id, cover_photo_url, thumbnail_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, province, city, country, street, post_code) 
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+      RETURNING *;
+  `, values).then(res => res.rows);
 }
 exports.addProperty = addProperty;
